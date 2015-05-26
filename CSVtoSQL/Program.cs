bb83b8a499cf6047f3ee5db9472d19da69ffa6d2
joshmasangcay89 <joshmasangcay89@gmail.com>
2015-05-27 02:07:17 +0800

    Added SELECT GROUP BY

--- CSVtoSQL/Program.cs
+++ CSVtoSQL/Program.cs
@@ -10,12 +10,8 @@ namespace CSVtoSQL
 {
     class Program
     {
-        static string Main()
+        static void Main()
         {
-            /// 
-            /// CSV Headers: Code PostDate, T, Description, RecTyp, PropAccountNmbr, Amount
-            ///
-
             string[] CSVs = Directory.GetFiles(@"C:\csv\");
             char[] delimiterChars = { ',' };
 
@@ -49,13 +45,14 @@ namespace CSVtoSQL
                         i++;
                     }                    
                 }
-                Console.ReadLine();
             }
             catch (Exception e)
             {
                 Console.WriteLine(">>>" + e.Message);
+                Console.ReadLine();
             }
-            return "Done";
+
+            GetJTRecords();            
         }
 
         public static void SaveToSQL(string filename_NE, string[] csvData, int csvDataLines, int lineno)
@@ -91,10 +88,163 @@ namespace CSVtoSQL
                 //    + csvData[6].Trim()); //Amount 
                 #endregion
 
-                Context.JT_Temp.Add(jt_temp);
-                Context.SaveChanges();                
+                //Context.JT_Temp.Add(jt_temp);
+                //Context.SaveChanges();      
+                //GetJTRecords();
             }
-            
         }
+
+        public static void GetJTRecords()
+        {
+            using (var Context = new AH_ODS_DBEntities())
+            {
+                //Goal:
+                //SELECT EnvelopeCode, Branch_COA, AQ_COA, AQ_Branch, SUM(Amount), AQ_PostStatus FROM JT_Temp
+                //GROUP BY EnvelopeCode, Branch_COA, AQ_COA, AQ_Branch, AQ_PostStatus
+
+                //var csvFilteredRecord = Context.JT_Temp.SqlQuery("SELECT * FROM JT_Temp").ToList<JT_Temp>(); 
+                // GROUP BY -- No go; Manual SELECT -- No go;
+                
+                try
+                {
+
+
+                    #region StackOverflow
+                    //var csvFilteredRecord = (
+                    //    from c in Context.JT_Temp
+                    //    group c by new
+                    //    {
+                    //        c.EnvelopeCode,
+                    //        c.Branch_COA,
+                    //        c.AQ_COA,
+                    //        c.AQ_Branch,
+                    //        c.AQ_PostStatus
+                    //    } into i
+                    //    select new JT_Temp
+                    //    {
+                    //        EnvelopeCode = i.Key.EnvelopeCode,
+                    //        Branch_COA = i.Key.Branch_COA,
+                    //        AQ_COA = i.Key.AQ_COA,
+                    //        AQ_Branch = i.Key.AQ_Branch,
+                    //        //TO-DO SUM(Amount),
+                    //        AQ_PostStatus = i.Key.AQ_PostStatus
+                    //    }).ToList(); 
+                    #endregion
+                    #region Group test Linq to Entity
+                    //var csvFilteredRecord = Context.JT_Temp
+                    //.GroupBy(g => new
+                    //{
+                    //    grp_env = g.EnvelopeCode,
+                    //    grp_branCOA = g.Branch_COA,
+                    //    grp_aqcoa = g.AQ_COA,
+                    //    grp_aqbranch = g.AQ_Branch,
+                    //    //grp_amount = g.Amount,
+                    //    grp_poststat = g.AQ_PostStatus
+                    //})
+                    //.Select(s => new JT_Temp 
+                    //{ 
+                    //    EnvelopeCode = s.Key.grp_env,
+                    //    Branch_COA = s.Key.grp_branCOA,
+                    //    AQ_COA = s.Key.grp_aqcoa,
+                    //    AQ_Branch = s.Key.grp_aqbranch,
+                    //    //SUM(Amount)
+                    //    AQ_PostStatus = s.Key.grp_poststat
+                    //})
+                    //.ToList(); 
+                    #endregion
+                    #region MyRegion
+                    //var csvFilteredRecord = (
+                    //            from c in Context.JT_Temp   
+                    //            group i by new
+                    //            {
+                    //                c.EnvelopeCode,
+                    //                c.Branch_COA,
+                    //                c.AQ_COA,
+                    //                c.AQ_Branch,
+                    //                c.AQ_PostStatus
+                    //            } into i
+
+                    //            select new JT_Temp
+                    //            {
+                    //                EnvelopeCode = i.Key.EnvelopeCode,
+                    //                Branch_COA = i.Key.Branch_COA,
+                    //                AQ_COA = i.Key.AQ_COA,
+                    //                AQ_Branch = i.Key.AQ_Branch,
+                    //                //sum(amount)
+                    //                AQ_PostStatus = i.Key.AQ_PostStatus
+                    //            }).ToList();
+                    
+                    //.AsEnumerable()
+                    //.Select(x => new JT_Temp
+                    //{
+                    //    EnvelopeCode = x.EnvelopeCode,
+                    //    Branch_COA = x.Branch_COA,
+                    //    AQ_COA = x.AQ_COA,
+                    //    AQ_Branch = x.AQ_Branch,
+                    //    //sum(amount)
+                    //    AQ_PostStatus = x.AQ_PostStatus
+                    //})
+                    //.ToList(); 
+                    #endregion
+
+                    var csvFilteredRecord = from con in Context.JT_Temp
+                                            group con by
+                                            con.EnvelopeCode
+                                            + con.Branch_COA
+                                            + con.AQ_COA
+                                            + con.AQ_Branch; ; 
+                                            //+ con.AQ_PostStatus;
+
+
+                    foreach (var Record in csvFilteredRecord)
+                    {
+                        Console.WriteLine(Record.Key
+                                //+ Record.Branch_COA
+                                //+ Record.AQ_COA
+                                ////+ Record.Amount
+                                //+ Record.AQ_PostStatus
+                            );
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("---------- " + e.Message);
+                    Console.ReadLine();
+             
+                }
+                #region Nope!
+
+                //foreach (var Record in xxx)
+                //{
+                //    Console.WriteLine(
+                //         Record.EnvelopeCode
+                //         + Record.Branch_COA
+                //         + Record.AQ_COA
+                //        //+ Record.Amount
+                //         + Record.AQ_PostStatus);
+                //}
+
+
+
+                ///working but no group by
+                //if (csvFilteredRecord.Count >= 1)
+                //{
+                //    //List<JT_Temp> result;
+                //    foreach (var Record in csvFilteredRecord)
+                //    {
+                //        Console.WriteLine(
+                //            Record.EnvelopeCode
+                //            + Record.Branch_COA
+                //            + Record.AQ_COA
+                //            + Record.Amount
+                //            + Record.AQ_PostStatus
+                //        );
+                //    }
+
+                //} 
+                #endregion
+            }
+        }
+
     }
 }
